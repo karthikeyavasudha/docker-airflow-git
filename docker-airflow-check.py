320d64ddcddb040a4ac201181b8b9a05e45ff0eb
karthikeyavasudha <142650489+karthikeyavasudha@users.noreply.github.com>
2025-03-10 15:10:41 +0530

    Add files via upload

---
+++ docker-airflow-check.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 3, 10),
+}
+
+with DAG('my_docker_git_dag', default_args=default_args, schedule_interval='@daily') as dag:
+    start = DummyOperator(task_id='start')
+    end = DummyOperator(task_id='end')
+
+    start >> end
